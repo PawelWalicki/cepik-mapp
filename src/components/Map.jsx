4ca1884bcb13da865d3b519656ff201ea4071972
Pawel Walicki <pawel.walicki89@interia.pl>
2024-12-02 16:21:30 +0100

    add map

---
+++ src/components/Map.jsx
@@ -0,0 +1,62 @@
+import "./Map.css"
+export const Map = () => {
+
+    return (
+        <div >
+            <div className="map">
+            <img className="map-img" src="/poland-map.png"></img>
+            <ul className="ul">
+                <li class="zachodnio-pomorskie li">
+                    <span class="visually-hidden">zachodnio-pomorskie</span>
+                </li>
+                <li class="pomorskie ">
+                    <span class="visually-hidden">pomorskie</span>
+                </li>
+                <li class="warminsko-mazurskie">
+                    <span class="visually-hidden">warmińsko-mazurskie</span>
+                </li>
+                <li class="podlaskie">
+                    <span class="visually-hidden">podlaskie</span>
+                </li>
+                <li class="lubuskie">
+                    <span class="visually-hidden">lubuskie</span>
+                </li>
+                <li class="wielkopolskie">
+                    <span class="visually-hidden">wielkopolskie</span>
+                </li>
+                <li class="kujawsko-pomorskie">
+                    <span class="visually-hidden">kujawsko-pomorskie</span>
+                </li>
+                <li class="mazowieckie">
+                    <span class="visually-hidden">mazowieckie</span>
+                </li>
+                <li class="dolnoslaskie">
+                    <span class="visually-hidden">dolnośląskie</span>
+                </li>
+                <li class="lodzkie">
+                    <span class="visually-hidden">łódzkie</span>
+                </li>
+                <li class="lubelskie">
+                    <span class="visually-hidden">lubleksie</span>
+                </li>
+                <li class="opolskie">
+                    <span class="visually-hidden">opolskie</span>
+                </li>
+                <li class="slaskie">
+                    <span class="visually-hidden">śląskie</span>
+                </li>
+                <li class="swietokrzyskie">
+                    <span class="visually-hidden">świętokrzyskie</span>
+                </li>
+                <li class="malopolskie">
+                    <span class="visually-hidden">małopoksie</span>
+                </li>
+                <li class="podkarpackie">
+                    <span class="visually-hidden">podkarpackie</span>
+                </li>
+            </ul>
+            </div>
+
+        </div>
+    )
+}
